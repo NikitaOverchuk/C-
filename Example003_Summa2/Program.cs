ca5852873f5172a3363a138be23003582a4ab858
Nikita Overchuk <nik.overchuk.97@mail.ru>
2022-08-17 17:42:41 +0300

    random

--- Example003_Summa2/Program.cs
+++ Example003_Summa2/Program.cs
@@ -1,3 +1,5 @@
-﻿int num1 = 65;
-int num2 = 67;
+﻿int num1 = new Random().Next(0, 10);
+int num2 = new Random().Next(0, 10);
+Console.WriteLine(num1);
+Console.WriteLine(num2);
 Console.WriteLine(num1 + num2);
