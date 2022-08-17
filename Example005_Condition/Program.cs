52897486afe6e1d0bc7b91d1a5e8154e97ed0c39
Nikita Overchuk <nik.overchuk.97@mail.ru>
2022-08-17 18:20:24 +0300

    ifElse

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,10 @@
+﻿string username = Console.ReadLine();
+if (username == "маша")
+{
+    Console.WriteLine("Это же МАША!");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
