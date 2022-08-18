6587b12b1a90bc428c290e9238dceadece2beffe
Nikita Overchuk <nik.overchuk.97@mail.ru>
2022-08-18 16:25:49 +0300

    find an element in an array

---
+++ Example008/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] num = {1, 2, 3, 4 ,5, 6, 7, 8, 9};
+int i = 0;
+int len = num.Length;
+int find = 8;
+while (i < len)
+{
+    if (num[i] == find)
+    {
+        Console.WriteLine(i);
+        break;
+    }
+    i++;
+}
