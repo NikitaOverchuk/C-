73c534f1705703c0b9e56c28abf45ee3edaea14e
Nikita Overchuk <nik.overchuk.97@mail.ru>
2022-08-19 21:13:51 +0300

    methods fill, print, find

---
+++ Example009/Program.cs
@@ -0,0 +1,44 @@
+﻿void printArray(int[] c)
+{
+    int len = c.Length;
+    int ind = 0;
+    while (ind < len)
+    {
+        Console.Write(c[ind] + " ");
+        ind++;
+    }
+}
+
+void fillArray(int[] arg)
+{
+    int len = arg.Length;
+    int i = 0;
+    while (i < len)
+    {
+        arg[i] = new Random().Next(-10, 11);
+        i++;
+    }
+}
+
+int findP(int[] array, int find)
+{
+    int len = array.Length;
+    int i = 0;
+    int pos = -1;
+    while (i < len)
+    {
+        if (array[i] == find)
+        {
+            pos = i;
+            break;
+        }
+        i++;
+    }
+    return pos;
+}
+
+int[] num = new int[10];
+fillArray(num);
+printArray(num);
+Console.WriteLine("");
+Console.WriteLine(findP(num, 4));
